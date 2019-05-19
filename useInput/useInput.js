9c2447f7ec06f123dca1b989c08410cd5a6e3c0d
seungsukim <seungsu0407@gmail.com>
2019-05-19 12:14:35 +0900

    ADD: useInput

---
+++ useInput/useInput.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = ({ target: { value } }) => {
+    const willUpdate = validator ? validator(value) : true;
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
+useInput.propTypes = {
+  initialValue: PropTypes.string.isRequired,
+  validator: PropTypes.func
+};
